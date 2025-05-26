fe519e262491af1e33e063c9a5db4c7fc0a76414
Qunainb <waniqunain@gmail.com>
2025-05-26 23:22:24 +0530

    Nested routes and showing Error page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./Pages/Home";
 import ProductPage from "./Pages/Product";
+import RootLayout from "./Pages/Root";
+import ErrorPage from "./Pages/Error";
 
 function App() {
   const router = createBrowserRouter([
-    { path: "/", element: <HomePage /> },
-    { path: "/products", element: <ProductPage /> },
+    {
+      path: "/",
+      element: <RootLayout />,
+      errorElement: <ErrorPage />,
+      children: [
+        { path: "/", element: <HomePage /> },
+        { path: "/products", element: <ProductPage /> },
+      ],
+    },
   ]);
   return <RouterProvider router={router} />;
 }
